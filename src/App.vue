def4485b64694970b6637b4f077fb6a228ebbd4c
Johannes Berggren <jb@learnlink.no>
2022-07-05 19:39:58 +0200

    Add firebase config.

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@ import { initializeApp } from 'firebase/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  //...
+  apiKey: "AIzaSyDcRk89s5vl4ihZ1J_cO0e6AOMDMIkXIz0",
+  authDomain: "s9bar-54d46.firebaseapp.com",
+  projectId: "s9bar-54d46",
+  storageBucket: "s9bar-54d46.appspot.com",
+  messagingSenderId: "815180929686",
+  appId: "1:815180929686:web:ac9bdb834bf375220540b7"
 };
 
 const app = initializeApp(firebaseConfig);
