c0ac20d37cde1f4b760a1b507dce7faec3870ce2
Johannes Berggren <jb@learnlink.no>
2022-07-05 20:04:06 +0200

    Firestore hookup.

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,52 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { initializeApp } from "firebase/app";
+import { collection, getDocs, getFirestore, query } from "firebase/firestore";
+import { onMounted, ref } from "vue";
 
-import { initializeApp } from 'firebase/app';
-
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDcRk89s5vl4ihZ1J_cO0e6AOMDMIkXIz0",
   authDomain: "s9bar-54d46.firebaseapp.com",
   projectId: "s9bar-54d46",
   storageBucket: "s9bar-54d46.appspot.com",
   messagingSenderId: "815180929686",
-  appId: "1:815180929686:web:ac9bdb834bf375220540b7"
+  appId: "1:815180929686:web:ac9bdb834bf375220540b7",
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const items = ref<Record<string, unknown>[]>([]);
+const members = ref<Record<string, unknown>[]>([]);
+
+onMounted(async () => {
+  const itemsQuery = query(collection(db, "items"));
+  const itemsSnapshot = await getDocs(itemsQuery);
+
+  itemsSnapshot.forEach((s) => {
+    items.value.push(s.data());
+  });
+
+  const membersQuery = query(collection(db, "members"));
+  const membersSnapshot = await getDocs(membersQuery);
+
+  membersSnapshot.forEach((s) => {
+    members.value.push(s.data());
+  });
+});
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img alt="S9 Logo" class="logo" src="./assets/logo.png" width="125" height="125" />
   </header>
 
   <main>
-    <TheWelcome />
+    <h4>Items</h4>
+    <pre style="margin-bottom: 50px">{{ items }}</pre>
+
+    <h4>Members</h4>
+    <pre>{{ members }}</pre>
   </main>
 </template>
 
