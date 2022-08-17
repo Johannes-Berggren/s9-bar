f7dd1e5b99a22010fe36a510046d31f7c408cce2
Johannes Berggren <jb@learnlink.no>
2022-08-17 20:07:11 +0200

    Correct URL

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://europe-west3-s9bar-54d46.cloudfunctions.net/api",
-  baseURL: "http://localhost:5001/s9bar-54d46/europe-west3/api",
+  baseURL: "https://europe-west3-s9bar-54d46.cloudfunctions.net/api",
+  // baseURL: "http://localhost:5001/s9bar-54d46/europe-west3/api",
   timeout: 60 * 1000,
   withCredentials: false,
 });
