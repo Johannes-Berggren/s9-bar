da50163922e059a3ed267b9be16606fca6acd92b
Johannes Berggren <jb@learnlink.no>
2022-10-11 15:50:19 +0200

    Sort items based on type, then name.

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -18,7 +18,10 @@ export async function getItem(id: number): Promise<Item> {
 }
 
 export async function getItems(): Promise<Item[]> {
-  const itemsSnapshot = await db.collection("items").orderBy("name", "asc").get();
+  const itemsSnapshot = await db.collection("items")
+    .orderBy("type", "asc")
+    .orderBy("name", "asc")
+    .get();
   return itemsSnapshot.docs.map((s) => s.data() as Item);
 }
 
