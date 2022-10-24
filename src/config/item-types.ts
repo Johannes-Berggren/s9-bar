bee1e11074368a95f880203b9e7127a6132b326c
Johannes Berggren <jb@learnlink.no>
2022-10-24 18:23:26 +0200

    Add soda item type.

--- src/config/item-types.ts
+++ src/config/item-types.ts
@@ -1,2 +1,2 @@
-const itemTypes = ["Beer", "Cider", "Candy", "Pre-mixed", "Non-alcoholic", "Seltzer", "Spirits"];
+const itemTypes = ["Beer", "Cider", "Candy", "Pre-mixed", "Non-alcoholic", "Seltzer", "Soda", "Spirits"];
 export default itemTypes;
